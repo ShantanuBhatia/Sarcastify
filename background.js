ae26b5ae7230a937032df78530bbc9207bcc496a
Shantanu Bhatia <sb5455@nyu.edu>
2019-11-05 10:05:07 +0530

    Removed unnecessary commented-out code

--- background.js
+++ background.js
@@ -5,15 +5,9 @@ browser.contextMenus.create({
 });
 browser.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === "sponge-mock") {
-        // Examples: text and HTML to be copied.
+        // Mixed-case version of highlighted text
         const text = sarcastify(info.selectionText);
-        // Always HTML-escape external input to avoid XSS.
-        // const safeUrl = escapeHTML(info.linkUrl);
-        // const html = `This is HTML: <a href="${safeUrl}">${safeUrl}</a>`;
-
-        // The example will show how data can be copied, but since background
-        // pages cannot directly write to the clipboard, we will run a content
-        // script that copies the actual content.
+        
 
         // clipboard-helper.js defines function copyToClipboard.
         const code = "copyToClipboard(" +
@@ -42,15 +36,7 @@ browser.contextMenus.onClicked.addListener((info, tab) => {
     }
 });
 
-// https://gist.github.com/Rob--W/ec23b9d6db9e56b7e4563f1544e0d546
-// function escapeHTML(str) {
-//     // Note: string cast using String; may throw if `str` is non-serializable, e.g. a Symbol.
-//     // Most often this is not the case though.
-//     return String(str)
-//         .replace(/&/g, "&amp;")
-//         .replace(/"/g, "&quot;").replace(/'/g, "&#39;")
-//         .replace(/</g, "&lt;").replace(/>/g, "&gt;");
-// }
+
 
 /*
 Go through the text and randomly decide whether to change a letter to uppercase or not. 
